f296e2dbaf0d6335f73a583f84ad9a429faab93b
Tim Dickeson II <imacubsfan23@gmail.com>
2017-11-16 02:04:17 -0600

    Fixed issues in python-i-o
    Make sure that if you create a "ticket.txt" file, that it is named "ticket.txt" and not "ticket.txt.txt", cause that's what I was doing for a while.

--- Pythong IO Homework.py
+++ Pythong IO Homework.py
@@ -2,22 +2,37 @@
 #CISI1400
 #Python File IO HW
 #Cory Schneider
-
 import os
 
-#Open and read file
 userchoice = input("Enter a filename: ").strip()
-infile = open("userchoice", "r")
-if  os.path.isfile("userchoice"):
 
+if os.path.isfile(userchoice):
+    infile = open(userchoice, "r")
     s = infile.read()
-
     tickets = [eval('x') for x in s.split()]
-
-
-#Open and write on output file
-output_file = open("output.txt", "w")
+    sum_a = []
+    for i in tickets:
+        try:
+            sum_a.append(float(i))
+        except ValueError:
+            next
+    print("**********************************")
+    print("          TICKET REPORT           ")
+    print("**********************************")
+    print("\nThere are ",len(tickets)," tickets in the database.\n")
+    print("Maximum Ticket price is $",max(sum_a))
+    print("Minimum Ticket price is $",min(sum_a))
+    print("Average Ticket price is $","{:.2f}".format(sum(sum_a)/len(tickets)),"\n")
+    print("Thank you for using our ticket system!\n")
+    print("**********************************\n")
+    infile.close()
+else:
+    print("Failed to open file")
     
+output_file = open("output.txt", "w")
+
 for line in userchoice:
     output_file.write(line + "\n")
-    output_file.write("hello")
+    output_file.write("hello\n")
+
+output_file.close()
